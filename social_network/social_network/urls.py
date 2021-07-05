0c440579ec8dd6f43abfabfd33d6a0be271b4f15
Aleksandr Honcharuk <alexandr.gnrk@gmail.com>
2021-07-06 01:20:29 +0300

    JWT auth configured.

--- social_network/social_network/urls.py
+++ social_network/social_network/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
-    url('auth/', include('djoser.urls.jwt')),
+    path('auth/', include('djoser.urls.jwt')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
