e7f7f88bbb4ee9273894d148703127b886d84c5f
Aleksandr Honcharuk <alexandr.gnrk@gmail.com>
2021-07-20 00:39:59 +0300

    Post like and unlike features added.

--- social_network/post/urls.py
+++ social_network/post/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, post_like, post_unlike
 
 urlpatterns = [
     path('', PostList.as_view()),
-    path('<int:pk>/', PostDetail.as_view())
+    path('<int:pk>/', PostDetail.as_view()),
+    path('<int:pk>/like/', post_like),
+    path('<int:pk>/unlike/', post_unlike),
 ]
--- social_network/post/views.py
+++ social_network/post/views.py
@@ -1,4 +1,6 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 from .models import Post
 from .serializers import PostSerializer
 
@@ -18,3 +20,29 @@ class PostDetail(generics.RetrieveAPIView):
 
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+@api_view(['POST'])
+@permission_classes([permissions.IsAuthenticated])
+def post_like(request, pk):
+    post = Post.objects.get(id=pk)
+    user = request.user
+
+    if user not in post.users_like.all():
+        post.users_like.add(user)
+        return Response(status=status.HTTP_200_OK)
+
+    return Response(status=status.HTTP_409_CONFLICT)
+
+
+@api_view(['POST'])
+@permission_classes([permissions.IsAuthenticated])
+def post_unlike(request, pk):
+    post = Post.objects.get(id=pk)
+    user = request.user
+
+    if user in post.users_like.all():
+        post.users_like.remove(user)
+        return Response(status=status.HTTP_200_OK)
+
+    return Response(status=status.HTTP_409_CONFLICT) 
