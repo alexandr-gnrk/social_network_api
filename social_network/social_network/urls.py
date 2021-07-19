70e853ac367e2be7f6b741a3fda6f8d6134ec89e
Aleksandr Honcharuk <alexandr.gnrk@gmail.com>
2021-07-19 18:05:47 +0300

    Auth logic was moved to appropriate app.

--- social_network/social_network/urls.py
+++ social_network/social_network/urls.py
@@ -15,15 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
-    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/auth', include('auth.urls')),
 ]
