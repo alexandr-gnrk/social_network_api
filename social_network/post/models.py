2beb372d216cb5f6c21c6118580530beeac48b9e
Aleksandr Honcharuk <alexandr.gnrk@gmail.com>
2021-07-19 19:16:40 +0300

    Post model created.

--- social_network/post/models.py
+++ social_network/post/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='created_posts',
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    created = models.DateField(auto_now_add=True, db_index=True)
+    users_like = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='liked_posts',
+        blank=True,
+    )
