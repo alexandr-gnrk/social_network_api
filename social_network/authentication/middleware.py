6643152c0c07526a852d6b4cd83b82e8a53266a7
Aleksandr Honcharuk <alexandr.gnrk@gmail.com>
2021-07-24 19:46:38 +0300

    Added custom middleware to track last user request time.

---
+++ social_network/authentication/middleware.py
@@ -0,0 +1,16 @@
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+
+class LastSeenUserMiddleware(object):
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.user.is_authenticated:
+            get_user_model().objects.\
+                filter(pk=request.user.pk).\
+                update(last_seen=timezone.now())
+
+        return self.get_response(request)
