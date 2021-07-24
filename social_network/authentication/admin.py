c9b28e8edb4208a20cb15fb6859fee68e5f7c128
Aleksandr Honcharuk <alexandr.gnrk@gmail.com>
2021-07-24 19:50:50 +0300

    Added last_seen field to User model and register this field in admin panel.

--- social_network/authentication/admin.py
+++ social_network/authentication/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
 
-admin.site.register(User, UserAdmin)
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        (None, {'fields': ('last_seen',)}),
+    )
